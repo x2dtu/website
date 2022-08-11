e942e445be3ffbb0a34ad7446f664ea913718dc9
michaelga <michaelga@vt.edu>
2022-08-11 03:09:18 -0400

    Updated xl size to 2000px

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,8 @@ import Head from "next/head";
 import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider } from "@emotion/react";
 import createEmotionCache from "../src/createEmotionCache";
+import { ThemeProvider } from "@mui/material";
+import theme from "../src/theme";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -19,11 +21,13 @@ export default function MyApp(props) {
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <div style={{ visibility: mounted ? "visible" : "hidden" }}>
-        <Component {...pageProps} />
-      </div>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <div style={{ visibility: mounted ? "visible" : "hidden" }}>
+          <Component {...pageProps} />
+        </div>
+      </ThemeProvider>
     </CacheProvider>
   );
 }
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 2000, // changed from default value of 1536
+    },
+  },
+});
+
+export default theme;
